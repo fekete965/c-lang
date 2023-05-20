7f55a64cff79de3d87f82bbe91dbd579a7762620
Bence Fekete <fekete965@gmail.com>
2023-05-20 15:50:37 +0100

    Add selectionSort

---
+++ selectionSort/selectionSort.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+void printNumbers(int *list, int size);
+void selectionSort(int *list, int size);
+void swap(int *a, int *b);
+
+int main()
+{
+  int nums[] = {8, 10, 6, 4, 5, 7, 3, 1, 9, 2};
+  int listSize = sizeof(nums) / sizeof(nums[0]);
+
+  printf("\n\t   Selection Sort \n");
+
+  printf("\nUnsorted list\n");
+  printNumbers(nums, listSize);
+
+  printf("\n");
+  selectionSort(nums, listSize);
+
+  printf("\nSorted list\n");
+  printNumbers(nums, listSize);
+
+  return 0;
+}
+
+void printNumbers(int *list, int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    printf(" %d ", list[i]);
+  }
+  printf("\n");
+}
+
+void selectionSort(int *list, int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    int minIdx = i;
+
+    for (int j = i + 1; j < size; j++)
+    {
+      if (list[minIdx] > list[j])
+      {
+        minIdx = j;
+      }
+    }
+
+    if (minIdx != i)
+    {
+      swap(&list[i], &list[minIdx]);
+    }
+
+    printf(" Step %-3d", i + 1);
+    printNumbers(list, size);
+  }
+}
+
+void swap(int *a, int *b)
+{
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
