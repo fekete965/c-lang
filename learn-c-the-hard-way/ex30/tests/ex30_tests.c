4f1d78b34939e714842dc492aeb75c857053ad56
Bence Fekete <fekete965@gmail.com>
2023-11-14 21:52:28 +0000

    Finish up ex30

--- learn-c-the-hard-way/ex30/tests/ex30_tests.c
+++ learn-c-the-hard-way/ex30/tests/ex30_tests.c
@@ -1,25 +1,56 @@
 #include "minuit.h"
+#include <dlfcn.h>
+
+typedef int (*lib_function)(const char *data, const int data_length);
+char *lib_file = "build/libex30.so";
+void *lib = NULL;
+
+int check_function(const char *func_to_run, const char *data, int data_length, int expected)
+{
+  lib_function func = dlsym(lib, func_to_run);
+  check(func != NULL, "Did not find %s function in the library %s: %s", func_to_run, lib_file, dlerror());
+
+  int rc = func(data, data_length);
+  check(rc == expected, "Function %s return %d for data %s with data length %d", func_to_run, rc, data, data_length);
+
+  return 1;
+
+error:
+  return 0;
+}
 
 char *test_dlopen()
 {
+  lib = dlopen(lib_file, RTLD_NOW);
+  mu_assert(lib != NULL, "Failed to open the library to test.");
 
   return NULL;
 }
 
 char *test_functions()
 {
+  mu_assert(check_function("print_a_message", "Hello", 5, 0),
+            "print_a_message failed.");
+  mu_assert(check_function("uppercase", "hello", 5, 0),
+            "uppercase failed.");
+  mu_assert(check_function("lowercase", "HELLO", 5, 0),
+            "lowercase failed");
 
   return NULL;
 }
 
 char *test_failures()
 {
+  mu_assert(check_function("fail_on_purpose", "Hello", 5, 1),
+            "fail_on_purpose should fail.");
 
   return NULL;
 }
 
 char *test_dlclose()
 {
+  int rc = dlclose(lib);
+  mu_assert(rc == 0, "Failed to close lib.");
 
   return NULL;
 }
